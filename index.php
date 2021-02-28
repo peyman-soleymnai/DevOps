34ad4c05314f80cecc900f16a2689aefb91b2c62
peyman-soleymani <soleymanip291@gmail.com>
2021-02-28 10:33:00 -0500

    chnaged

--- index.php
+++ index.php
@@ -307,7 +307,7 @@ switch (ENVIRONMENT)
 
 /*
  * --------------------------------------------------------------------
- * LOAD THE BOOTSTRAP FILE
+ * LOAD THE BOOTSTRAP FILE IN PHP_VERSION
  * --------------------------------------------------------------------
  *
  * And away we go...
